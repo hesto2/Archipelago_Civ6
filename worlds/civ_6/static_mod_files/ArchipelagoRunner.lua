81fc5bd663b2926dba0edd763ef2f6f3db5c46a3
hesto2 <hestermancarter@gmail.com>
2024-04-22 19:04:53 -0600

    Add get checked locations

--- worlds/civ_6/static_mod_files/ArchipelagoRunner.lua
+++ worlds/civ_6/static_mod_files/ArchipelagoRunner.lua
@@ -7,13 +7,21 @@ HUMAN_PLAYER = nil
 RECEIVED_ITEMS = {}
 -- {0: {Name: "TECH_POTTERY", Sender: "Mike"}}
 
+-- Messages read by the client will be started/ended with these
+CLIENT_PREFIX = "APSTART:"
+CLIENT_POSTFIX = ":APEND"
+
 CHECKED_LOCATIONS = {}
 
 TECH_NOTIFICATION_TYPE = 5
 CIVIC_NOTIFICATION_TYPE = 12
 
+-- Sets to true once a turn begins, this is to let the client know that the game can be interacted with now
+IS_IN_GAME = false
+
 function OnTurnBegin()
     print("START OnTurnBegin")
+    IS_IN_GAME = true
     for key, item in pairs(RECEIVED_ITEMS) do
         GiveItemToPlayer(item, key)
     end
@@ -69,16 +77,39 @@ end
 
 function GetCheckedLocations()
     print("START GetCheckedLocations")
+    locations = {}
+    for key, value in pairs(TECHS) do
+        id = key - 1
+        if id >= TECH_BLOCKER_ID and HUMAN_PLAYER:GetTechs():HasTech(id) then
+            table.insert(locations, 1, value.TechnologyType)
+        end
+    end
+    for key, value in pairs(CIVICS) do
+        id = key - 1
+        if id >= CIVIC_BLOCKER_ID and HUMAN_PLAYER:GetCulture():HasCivic(id) then
+            table.insert(locations, 1, value.CivicType)
+        end
+    end
     print("END GetCheckedLocations")
+    return CLIENT_PREFIX .. table.concat(locations, ",") .. CLIENT_POSTFIX
+end
+
+function IsInGame()
+    return IS_IN_GAME
 end
 
 function Init()
     print("Running Main")
     print("Adding turn begin")
+    -- Events to listen for
     Events.TurnBegin.Add(OnTurnBegin);
+
+    -- Functions to expose to the client
     Game.ReceiveItem = ReceiveItem
     Game.GetCheckedLocations = GetCheckedLocations
+    Game.IsInGame = IsInGame
 
+    -- Initialize the techs
     TECHS = DB.Query("Select * FROM Technologies")
 
     for key, value in pairs(TECHS) do
@@ -86,6 +117,7 @@ function Init()
             TECH_BLOCKER_ID = key - 1 -- DB index starts at 1, ids start at 0
         end
     end
+    -- Initialize civics
     CIVICS = DB.Query("Select * FROM Civics")
     for key, value in pairs(CIVICS) do
         if value.CivicType == "CIVIC_BLOCKER" then
@@ -93,6 +125,7 @@ function Init()
         end
     end
 
+    -- Give AP techs/civics to AI so they aren't tempted to research them for no reason
     PLAYERS = Game.GetPlayers()
     for key, player in pairs(PLAYERS) do
         if player:IsHuman() == false then
